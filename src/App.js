9a4634c297f506a3a602befd82a8792b46abb35c
fikriansyah12 <fikriansyahmuhammad664.com>
2024-02-26 01:36:07 +0700

    x commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormStructor from './components/FormStructor';
+
+
+function App() {
+  return (
+    <>
+        <FormStructor/>
+    </>
+  );
+}
+
+export default App;
